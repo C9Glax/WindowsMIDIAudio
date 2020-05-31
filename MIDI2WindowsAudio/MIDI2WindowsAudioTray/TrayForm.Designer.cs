78b02652b4d34e18f22f5975b0a689ccd02ae8f0
Glax <gfreak70@gmail.com>
2020-05-31 23:38:32 +0200

    Added TrayApplication

---
+++ MIDI2WindowsAudio/MIDI2WindowsAudioTray/TrayForm.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace MIDI2WindowsAudioTray
+{
+    partial class TrayForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrayForm));
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBoxAudioDevices = new System.Windows.Forms.ListBox();
+            this.lblAudioDevices = new System.Windows.Forms.Label();
+            this.btnAddControl = new System.Windows.Forms.Button();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.lblVolumeControl = new System.Windows.Forms.Label();
+            this.lblMuteControl = new System.Windows.Forms.Label();
+            this.txtMute = new System.Windows.Forms.TextBox();
+            this.listBoxMidiOut = new System.Windows.Forms.ListBox();
+            this.listBoxMidiIn = new System.Windows.Forms.ListBox();
+            this.lblMidiIn = new System.Windows.Forms.Label();
+            this.lblMidiOut = new System.Windows.Forms.Label();
+            this.lblControl = new System.Windows.Forms.Label();
+            this.contextMenuStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLoad,
+            this.toolStripSave,
+            this.toolStripExit});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(101, 70);
+            // 
+            // toolStripLoad
+            // 
+            this.toolStripLoad.Enabled = false;
+            this.toolStripLoad.Name = "toolStripLoad";
+            this.toolStripLoad.Size = new System.Drawing.Size(180, 22);
+            this.toolStripLoad.Text = "Load";
+            this.toolStripLoad.Click += new System.EventHandler(this.LoadFile_Click);
+            // 
+            // toolStripSave
+            // 
+            this.toolStripSave.Enabled = false;
+            this.toolStripSave.Name = "toolStripSave";
+            this.toolStripSave.Size = new System.Drawing.Size(180, 22);
+            this.toolStripSave.Text = "Save";
+            this.toolStripSave.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // toolStripExit
+            // 
+            this.toolStripExit.Name = "toolStripExit";
+            this.toolStripExit.Size = new System.Drawing.Size(100, 22);
+            this.toolStripExit.Text = "Exit";
+            this.toolStripExit.Click += new System.EventHandler(this.toolStripExit_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "MIDI2WindowsAudio";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Enabled = false;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Load";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.LoadFile_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // listBoxAudioDevices
+            // 
+            this.listBoxAudioDevices.Enabled = false;
+            this.listBoxAudioDevices.FormattingEnabled = true;
+            this.listBoxAudioDevices.Location = new System.Drawing.Point(221, 55);
+            this.listBoxAudioDevices.Name = "listBoxAudioDevices";
+            this.listBoxAudioDevices.Size = new System.Drawing.Size(300, 212);
+            this.listBoxAudioDevices.TabIndex = 2;
+            // 
+            // lblAudioDevices
+            // 
+            this.lblAudioDevices.AutoSize = true;
+            this.lblAudioDevices.Location = new System.Drawing.Point(218, 39);
+            this.lblAudioDevices.Name = "lblAudioDevices";
+            this.lblAudioDevices.Size = new System.Drawing.Size(73, 13);
+            this.lblAudioDevices.TabIndex = 3;
+            this.lblAudioDevices.Text = "AudioDevices";
+            // 
+            // btnAddControl
+            // 
+            this.btnAddControl.Enabled = false;
+            this.btnAddControl.Location = new System.Drawing.Point(527, 124);
+            this.btnAddControl.Name = "btnAddControl";
+            this.btnAddControl.Size = new System.Drawing.Size(78, 23);
+            this.btnAddControl.TabIndex = 4;
+            this.btnAddControl.Text = "Bind";
+            this.btnAddControl.UseVisualStyleBackColor = true;
+            this.btnAddControl.Click += new System.EventHandler(this.btnAddControl_Click);
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Enabled = false;
+            this.txtVolume.Location = new System.Drawing.Point(527, 55);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(78, 20);
+            this.txtVolume.TabIndex = 5;
+            // 
+            // lblVolumeControl
+            // 
+            this.lblVolumeControl.AutoSize = true;
+            this.lblVolumeControl.Location = new System.Drawing.Point(527, 39);
+            this.lblVolumeControl.Name = "lblVolumeControl";
+            this.lblVolumeControl.Size = new System.Drawing.Size(78, 13);
+            this.lblVolumeControl.TabIndex = 6;
+            this.lblVolumeControl.Text = "Volume Control";
+            // 
+            // lblMuteControl
+            // 
+            this.lblMuteControl.AutoSize = true;
+            this.lblMuteControl.Location = new System.Drawing.Point(527, 82);
+            this.lblMuteControl.Name = "lblMuteControl";
+            this.lblMuteControl.Size = new System.Drawing.Size(67, 13);
+            this.lblMuteControl.TabIndex = 8;
+            this.lblMuteControl.Text = "Mute Control";
+            // 
+            // txtMute
+            // 
+            this.txtMute.Enabled = false;
+            this.txtMute.Location = new System.Drawing.Point(527, 98);
+            this.txtMute.Name = "txtMute";
+            this.txtMute.Size = new System.Drawing.Size(78, 20);
+            this.txtMute.TabIndex = 7;
+            // 
+            // listBoxMidiOut
+            // 
+            this.listBoxMidiOut.Enabled = false;
+            this.listBoxMidiOut.FormattingEnabled = true;
+            this.listBoxMidiOut.Location = new System.Drawing.Point(12, 172);
+            this.listBoxMidiOut.Name = "listBoxMidiOut";
+            this.listBoxMidiOut.Size = new System.Drawing.Size(203, 95);
+            this.listBoxMidiOut.TabIndex = 9;
+            this.listBoxMidiOut.SelectedIndexChanged += new System.EventHandler(this.listBoxMidiOut_SelectedIndexChanged);
+            // 
+            // listBoxMidiIn
+            // 
+            this.listBoxMidiIn.FormattingEnabled = true;
+            this.listBoxMidiIn.Location = new System.Drawing.Point(12, 55);
+            this.listBoxMidiIn.Name = "listBoxMidiIn";
+            this.listBoxMidiIn.Size = new System.Drawing.Size(203, 82);
+            this.listBoxMidiIn.TabIndex = 10;
+            this.listBoxMidiIn.SelectedIndexChanged += new System.EventHandler(this.listBoxMidiIn_SelectedIndexChanged);
+            // 
+            // lblMidiIn
+            // 
+            this.lblMidiIn.AutoSize = true;
+            this.lblMidiIn.Location = new System.Drawing.Point(12, 39);
+            this.lblMidiIn.Name = "lblMidiIn";
+            this.lblMidiIn.Size = new System.Drawing.Size(42, 13);
+            this.lblMidiIn.TabIndex = 11;
+            this.lblMidiIn.Text = "MIDI-In";
+            // 
+            // lblMidiOut
+            // 
+            this.lblMidiOut.AutoSize = true;
+            this.lblMidiOut.Location = new System.Drawing.Point(12, 156);
+            this.lblMidiOut.Name = "lblMidiOut";
+            this.lblMidiOut.Size = new System.Drawing.Size(50, 13);
+            this.lblMidiOut.TabIndex = 12;
+            this.lblMidiOut.Text = "MIDI-Out";
+            // 
+            // lblControl
+            // 
+            this.lblControl.AutoSize = true;
+            this.lblControl.Location = new System.Drawing.Point(527, 156);
+            this.lblControl.Name = "lblControl";
+            this.lblControl.Size = new System.Drawing.Size(71, 13);
+            this.lblControl.TabIndex = 13;
+            this.lblControl.Text = "Last Pressed:";
+            // 
+            // TrayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 279);
+            this.Controls.Add(this.lblControl);
+            this.Controls.Add(this.lblMidiOut);
+            this.Controls.Add(this.lblMidiIn);
+            this.Controls.Add(this.listBoxMidiIn);
+            this.Controls.Add(this.listBoxMidiOut);
+            this.Controls.Add(this.lblMuteControl);
+            this.Controls.Add(this.txtMute);
+            this.Controls.Add(this.lblVolumeControl);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.btnAddControl);
+            this.Controls.Add(this.lblAudioDevices);
+            this.Controls.Add(this.listBoxAudioDevices);
+            this.Controls.Add(this.menuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "TrayForm";
+            this.ShowIcon = false;
+            this.Text = "MIDI2WindowsAudio";
+            this.Shown += new System.EventHandler(this.TrayForm_Shown);
+            this.Resize += new System.EventHandler(this.TrayForm_Resize);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ToolStripMenuItem toolStripLoad;
+        private System.Windows.Forms.ToolStripMenuItem toolStripSave;
+        private System.Windows.Forms.ToolStripMenuItem toolStripExit;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBoxAudioDevices;
+        private System.Windows.Forms.Label lblAudioDevices;
+        private System.Windows.Forms.Button btnAddControl;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Label lblVolumeControl;
+        private System.Windows.Forms.Label lblMuteControl;
+        private System.Windows.Forms.TextBox txtMute;
+        private System.Windows.Forms.ListBox listBoxMidiOut;
+        private System.Windows.Forms.ListBox listBoxMidiIn;
+        private System.Windows.Forms.Label lblMidiIn;
+        private System.Windows.Forms.Label lblMidiOut;
+        private System.Windows.Forms.Label lblControl;
+    }
+}
+
---
+++ MIDI2WindowsAudio/MIDI2WindowsAudioTray/TrayForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsSoundControl;
+using MIDI2WindowsAudio;
+using MidiAccess;
+
+namespace MIDI2WindowsAudioTray
+{
+    public partial class TrayForm : Form
+    {
+        private AudioDeviceInfo.DeviceInfo[] devices;
+        private MIDI2Win controller;
+        private string midiIn, midiOut;
+
+        public TrayForm()
+        {
+            InitializeComponent();
+            this.listBoxMidiIn.Items.AddRange(MidiInformation.ListInputDevices());
+            this.listBoxMidiOut.Items.AddRange(MidiInformation.ListOutputDevices());
+        }
+
+        private void LoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "TEXT (*.txt)|All files (*.*)";
+            dialog.FilterIndex = 0;
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+                this.controller.ImportSettings(dialog.FileName);
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "TEXT (*.txt)|All files (*.*)";
+            dialog.FilterIndex = 0;
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+                this.controller.ExportSettings(dialog.FileName);
+
+        }
+
+        private void toolStripExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TrayForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                this.Hide();
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void TrayForm_Shown(object sender, EventArgs e)
+        {
+            this.listBoxAudioDevices.Items.Add("Loading Audio Devices...");
+            this.devices = AudioDeviceInfo.GetAudioDevices();
+            this.listBoxAudioDevices.Items.Clear();
+            foreach (AudioDeviceInfo.DeviceInfo device in devices)
+            {
+                this.listBoxAudioDevices.Items.Add(device);
+            }
+        }
+
+        private void listBoxMidiIn_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listBoxMidiIn.SelectedIndex == -1)
+                return;
+            this.midiIn = this.listBoxMidiIn.SelectedItem.ToString();
+            this.listBoxMidiIn.Enabled = false;
+            this.listBoxMidiOut.Enabled = true;
+        }
+
+        private void listBoxMidiOut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listBoxMidiOut.SelectedIndex == -1)
+                return;
+            this.midiOut = this.listBoxMidiOut.SelectedItem.ToString();
+            this.controller = new MIDI2Win(Controller.FilterType.Name, this.midiIn, this.midiOut);
+            this.controller.OnMidi += Controller_OnMidi;
+            this.listBoxMidiOut.Enabled = false;
+            this.listBoxAudioDevices.Enabled = true;
+            this.btnAddControl.Enabled = true;
+            this.fileToolStripMenuItem.Enabled = true;
+            this.txtMute.Enabled = true;
+            this.txtVolume.Enabled = true;
+            this.toolStripLoad.Enabled = true;
+            this.toolStripSave.Enabled = true;
+        }
+
+        private void btnAddControl_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string guid = ((AudioDeviceInfo.DeviceInfo)this.listBoxAudioDevices.SelectedItem).guid;
+                int volumecontrol = Convert.ToInt32(this.txtVolume.Text);
+                int mutecontrol = Convert.ToInt32(this.txtMute.Text);
+                controller.AddControl(volumecontrol, mutecontrol, guid);
+            }
+            catch (FormatException) { }
+        }
+
+        private void Controller_OnMidi(object sender, MIDI2Win.MidiControlArgs e)
+        {
+            this.lblControl.Invoke((MethodInvoker) delegate () { this.lblControl.Text = string.Format("Last Pressed: {0}", e.control.ToString()); });
+        }
+    }
+}
