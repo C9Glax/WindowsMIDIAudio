94b94e2e7601b8e3af0d889cc367924b0c9a03d3
C9Glax <13404778+C9Glax@users.noreply.github.com>
2023-03-06 02:31:43 +0100

    Added GUI

---
+++ MIDI2WindowsAudio/KorgNanokontrol2MWAudioGUI/ConsoleWriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace KorgNanokontrol2MWAudioGUI;
+
+public class ConsoleWriter : TextWriter
+{
+    public override Encoding Encoding { get; }
+    public event EventHandler<ConsoleWriterEventArgs>? OnWrite;
+    public event EventHandler<ConsoleWriterEventArgs>? OnWriteLine;
+
+    public ConsoleWriter()
+    {
+        Encoding = Encoding.UTF8;
+    }
+
+    public override void Write(string? text)
+    {
+        if(text is not null)
+            this.OnWrite?.Invoke(this, new ConsoleWriterEventArgs(text));
+        base.Write(text);
+    }
+
+    public override void WriteLine(string? text)
+    {
+        if (text is not null)
+            this.OnWriteLine?.Invoke(this, new ConsoleWriterEventArgs(text));
+        base.WriteLine(text);
+    }
+}
+
+public class ConsoleWriterEventArgs : EventArgs
+{
+    public string text { get; }
+    public ConsoleWriterEventArgs(string text)
+    {
+        this.text = text;
+    }
+}
